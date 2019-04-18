8d3ed997ab5876a542f63dc8958e27c70134eee7
Yashodhan Kulkarni <ypkulkarni@ucdavis.edu>
2019-04-17 18:22:52 -0700

    update current weather info

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,9 @@ orscFunction = function(){
     if(xhr.readyState == 4){ // Request is done
         if(xhr.status == 200){ // Everything is smooth
             object = JSON.parse(xhr.responseText);
-            printDisplay(object);
+            //printDisplay(object);
+            updateScreen(object);
+            //console.log(object);
         }
         if(xhr.status == 404){
             console.log("Not found");
@@ -22,8 +24,8 @@ orscFunction = function(){
     }
 };
 
-//xhr = makeCorsRequest();
-//xhr.onreadystatechange = orscFunction;
+xhr = makeCorsRequest();
+xhr.onreadystatechange = orscFunction;
 
 
 
@@ -40,5 +42,84 @@ function printDisplay(obj){
     for(let i = 1; i < 6; i++){
         console.log(`Hour${i}: ${((today.getHours()+i)%12 == 0)?12:(today.getHours()+i)%12}`)
     }
+}
+
+function updateScreen(obj){
+    //Update current weather
+    const currentTemp = document.querySelector("#current-temp");
+    currentTemp.textContent = Math.floor(obj.list[0].main.temp);
+
+    const currentTime = document.querySelector("#main-time");
+    currentTime.textContent = getTimeStr(today.getHours());
+
+    const currentIcon = document.querySelector("#current-icon");
+    currentIcon.src = getImgUrl(obj.list[0].weather[0].icon);
+
+}
+
+function getTimeStr(tm){
+    tm = tm % 24;
+    let str = "";
+
+    if(tm == 0){
+        str = "12 AM";
+    } else if(tm == 12){
+        str = "12 PM";
+    } else{
+        const ampm = (tm > 12)?"PM":"AM";
+        str = `${tm%12} ${ampm}`;
+    }
+
+    return str;
+}
+
+function getImgUrl(iconId){
+    let imgUrl = "";
+
+    switch(iconId){
+        case "01d":
+            imgUrl = "assets/img/clearsky.svg";
+            break;
+        case "01n":
+            imgUrl = "assets/img/clear-night.svg";
+            break;
+        case "02d":
+            imgUrl = "assets/img/fewclouds-day.svg";
+            break;
+        case "02n":
+            imgUrl = "assets/img/fewclouds-night.svg";
+            break;
+        case "03d":          
+        case "03n":
+            imgUrl = "assets/img/scatteredclouds.svg";
+            break;
+        case "04d":
+        case "04n":
+            imgUrl = "assets/img/brokencloud.svg";
+            break;
+        case "09d":
+        case "09n":
+            imgUrl = "assets/img/showerrain.svg";
+            break;
+        case "10d":
+            imgUrl = "assets/img/rain-day.svg";
+            break;
+        case "10n":
+            imgUrl = "assets/img/rain-night.svg";
+            break;
+        case "11d":
+        case "11n":
+            imgUrl = "assets/img/thunderstorms.svg";
+            break;
+        case "13d":
+        case "13n":
+            imgUrl = "assets/img/snow.svg";
+            break;
+        case "50d":
+        case "50n":
+            imgUrl = "assets/img/mist.svg";
+            break;
+    }
+    return imgUrl;
 }
 
