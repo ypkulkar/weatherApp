1d3a06d5d2e6d5b43998514ed70a465dced6eab9
Yashodhan Kulkarni <ypkulkarni@ucdavis.edu>
2019-04-24 10:18:04 -0700

    Can check city distance now and says not found

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,14 @@ orscFunction = function(){
         if(xhr.status == 200){ // Everything is smooth
             object = JSON.parse(xhr.responseText);
             //printDisplay(object);
-            updateScreen(object);
-            //console.log(object);
+            if(!checkDist(object)){
+                alert("Not Found");
+            }
+            else{
+                updateScreen(object);
+            }
+            // console.log(object);
+            //checkDist(object);
         }
         if(xhr.status == 404){
             console.log("Not found");
@@ -191,3 +197,43 @@ function getImgUrl(iconId){
     return imgUrl;
 }
 
+function checkDist(obj){
+    // get coordinates
+    let lat1 = obj.city.coord["lat"];
+    let lon1 = obj.city.coord["lon"];
+    let lat2 = 38.5449;
+    let lon2 = -121.7405;
+    let dist = getDistInMiles(lat1,lon1,lat2,lon2);
+    console.log(dist);
+    //bool
+    let ret = (dist < 151)?true:false;
+    console.log(ret);
+    
+
+
+    return ret;
+    // use function below
+}
+
+
+// https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+function getDistInMiles(lat1, lon1, lat2, lon2) {
+    let R = 6371; // Radius of the earth in km
+    let dLat = deg2rad(lat2-lat1);  // deg2rad below
+    let dLon = deg2rad(lon2-lon1); 
+    let a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    let d = R * c; // Distance in km
+    d = d * 0.62137119; // Distance in miles
+    return d;
+  }
+  
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
+  
